701916517ffa183e7ed152f569d628c77d5ba29a
caloloc2 <calolomino@gmail.com>
2022-06-23 11:58:33 -0500

    actualizacion

--- app/src/main/java/com/nibemi/controluisrael/PrincipalActivity.java
+++ app/src/main/java/com/nibemi/controluisrael/PrincipalActivity.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PrincipalActivity extends AppCompatActivity {
 
     Button btn_cerrar;
     Button btn_bloqueo, btn_localizar, btn_ver_imagenes, btn_activar_seguros;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRefLocacion = database.getReference("locacion");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,9 @@ public class PrincipalActivity extends AppCompatActivity {
         btn_ver_imagenes = (Button)findViewById(R.id.btn_ver_imagenes);
         btn_localizar = (Button)findViewById(R.id.btn_localizar);
 
+        final int[] bloqueo = {0};
+        final int[] activacion = {0};
+
         btn_cerrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,7 +50,15 @@ public class PrincipalActivity extends AppCompatActivity {
         btn_bloqueo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                DatabaseReference myRef = database.getReference("bloqueo");
 
+                myRef.setValue(bloqueo[0]);
+
+                if (bloqueo[0] == 0){
+                    bloqueo[0] = 1;
+                }else{
+                    bloqueo[0] = 0;
+                }
             }
         });
 
@@ -44,6 +66,15 @@ public class PrincipalActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                DatabaseReference myRef = database.getReference("activacion");
+
+                myRef.setValue(activacion[0]);
+
+                if (activacion[0] == 0){
+                    activacion[0] = 1;
+                }else{
+                    activacion[0] = 0;
+                }
             }
         });
 
@@ -57,7 +88,18 @@ public class PrincipalActivity extends AppCompatActivity {
         btn_localizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                myRefLocacion.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String value = dataSnapshot.getValue(String.class);
+                        Toast.makeText(getApplicationContext(), value, Toast.LENGTH_LONG).show();
+                    }
 
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        Log.w("DEBUG", "Failed to read value.", error.toException());
+                    }
+                });
             }
         });
     }
