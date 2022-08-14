c6af8c83e8ad2cd2e0d6b6b136c750b4452ffa19
caloloc2 <calolomino@gmail.com>
2022-08-14 17:52:20 -0500

    actualizacion

--- app/src/main/java/com/nibemi/controluisrael/MainActivity.java
+++ app/src/main/java/com/nibemi/controluisrael/MainActivity.java
@@ -2,6 +2,9 @@ package com.nibemi.controluisrael;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,9 +20,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.concurrent.Executor;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_login;
+    Button btn_login, btn_huella;
     TextView txt_username, txt_password;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRefLogin = database.getReference("inicio");
@@ -29,9 +34,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BiometricManager biometricManager = BiometricManager.from(getApplicationContext());
+        switch (biometricManager.canAuthenticate()){
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                Log.d("DEBUG", "LA APP PUEDE USAR HUELLA COMO LOGIN");
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                btn_huella.setVisibility(View.GONE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                btn_huella.setVisibility(View.GONE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                btn_huella.setVisibility(View.GONE);
+                break;
+        }
+
+        Executor executor = ContextCompat.getMainExecutor(this);
+
+        BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+//                Toast.makeText(getApplicationContext(), "Inicio correctamente con huella", Toast.LENGTH_SHORT).show();
+                finish();
+                Intent intent = new Intent(getApplicationContext(), PrincipalActivity.class);
+                startActivity(intent);
+            }
+
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Login")
+                .setDescription("Iniciar sesión con huella dactilar")
+                .setNegativeButtonText("Cancelar")
+                .build();
+
+
+
         txt_username = (TextView) findViewById(R.id.txt_username);
         txt_password = (TextView) findViewById(R.id.txt_password);
+        btn_huella = (Button) findViewById(R.id.btn_huella);
         btn_login = (Button) findViewById(R.id.btn_logueo);
+
+        btn_huella.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(promptInfo);
+            }
+        });
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
